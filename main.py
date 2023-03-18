9360ed963baa17708796ac73ead6769417e24224
JINJOO PARK <117656236+lolo-park@users.noreply.github.com>
2023-03-18 23:21:50 +0900

    아직 푸는 중
    아직 푸는 중

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    a = 1
+    b = 2
+
+
+    a = 4
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'answer = {a + b}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
