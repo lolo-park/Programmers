6794f029a01393867645d737d0464e69c1516f8f
JINJOO PARK <117656236+lolo-park@users.noreply.github.com>
2023-03-20 16:43:40 +0900

    Update if.py
    if
    pass
    else

--- programmers_problems/if.py
+++ programmers_problems/if.py
@@ -1,13 +1,21 @@
+"""
 money = False
 
 if money:
     print("택시")
 else:
     print("걷기")
-    
+
 money = True
 
 if money:
     print("돈있으면 택시")
 else:
     print("돈 없으면 걷기")
+"""
+pocket = ['paper', 'cellphone']
+if 'money' in pocket:
+    pass
+else:
+    print("카드를 꺼내라")
+
