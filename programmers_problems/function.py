68c0ea301fe1a743603be2c8cc4c82aa6aca8d21
JINJOO PARK <117656236+lolo-park@users.noreply.github.com>
2023-03-23 09:55:14 +0900

    slight change

--- programmers_problems/function.py
+++ programmers_problems/function.py
@@ -1,4 +1,21 @@
-# 1 - print를 활용한 데이터 입출력
+# 1 - 함수의 정의 definition
+# (1) return 값이 있는 함수
+def add(a, b):
+    result = a + b
+    return result
+# (2) print( ) 함수를 사용하기
+def say():
+    return 'print(say())로 출력 ======= Helllo'
+print(say())
+# (3) return 값이 없는 함수
+def addy(a, b):
+    print("%d, %d의 합은 %d입니다" %(a, b, a+b))
+addy(3, 4)
+
+
+
+
+# 2 - print를 활용한 데이터 입출력
 
 print("life", "is", "too short") # => life is too short
 
@@ -8,7 +25,7 @@ print("life" "is" "too short") # => lifeistooshort
 for i in range(10):
     print(i, end='') # => 0123456789
 
-# 2 - 파일을 통한 입출력 방법
+# 3 - 파일을 통한 입출력 방법
 f = open("new_file.txt", 'w')
 for i in range(1, 11):
     data = "%d th line.\n" % i
