ac193f66f82b5f90d08fb2a564434ba5bccc9ec3
JINJOO PARK <117656236+lolo-park@users.noreply.github.com>
2023-06-06 12:30:13 +0900

    UPDATE max_value.py

--- programmers/max_value.py
+++ programmers/max_value.py
@@ -4,7 +4,7 @@ numbers의 원소 중 두 개를 곱해 만들 수 있는
 최댓값을 return하도록 solution 함수를 완성해주세요.
 """
 
-
+# 나의 풀이
 def solution(numbers):
   answer = sorted(numbers)
   max_value = answer[len(answer)-1] *answer[len(answer)-2]
@@ -13,7 +13,6 @@ def solution(numbers):
 
 # 위와 같이 푸는 것이 가장 기본적인 접근 방법일 것 같은데 상당히 찝찝하게 생겼음.
 # 좀 더 스마트한 접근방법은 없을까
-
 def solution(numbers):
 
   a = max(numbers) # numbers 배열에서 가장 큰 숫자를 a 변수에 저장
@@ -22,5 +21,15 @@ def solution(numbers):
 
   return a * b
   # 근데 여기서 만약 가장 큰 숫자가 중복이었다면 remove()에서는 하나만 지워짐 (가장앞에있는)
-  # 아 하긴 어차피 상관없는게 가장 큰 숫자 두개 곱하는 문제여도 위에 알고리즘으로 가능하다! 
+  # 아 하긴 어차피 상관없는게 가장 큰 숫자 두개 곱하는 문제여도 위에 알고리즘으로 가능하다!
+
+
+# 다른 사람 풀이
+def solution(numbers):
+  numbers.sort() # 리스트 자체를 순서대로 재정렬
+  answer = numbers[-2] * numbers[-1] # [-1], [-2]로 뒤에서부터 인덱스에 접근
+  return answer
+
+
+
 
