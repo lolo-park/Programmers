db480d175d9fadc41d9823d8aa7c0b6058ca20ce
JINJOO PARK <117656236+lolo-park@users.noreply.github.com>
2023-06-07 12:51:56 +0900

    for문 말고 리스트의 index 번호 구하기

--- programmers/height.py
+++ programmers/height.py
@@ -17,9 +17,20 @@ height = 정수(머쓱이의 키)
 def solution(array, height):
     answer = []
 
-    for heights in array:
+    for heights in array: # for문은 index[0]부터 scan 해서 많아질 수록 부담이 될 수도
         if heights > height:
             answer.append(heights)
 
     return len(answer)
 # 진짜 계속 이렇게 전통적인 방식으로밖에 접근이 안된다 하하하
+
+
+def solution(array, height):
+    array.append(height) # height를 바로 array에 추가시킨다
+    array.sort(reverse=True) # 큰 숫자부터 정렬될 수 있도록 sort(reverse = True)
+    return array.index(height) # array 리스트에서 height의 index 번호를 return 한다
+# return 전에 height가 170이고 array가 [183, 182, 181, 180,170,160] 이었다면,
+# height(170)의 index는 4
+# 리스트의 index 번호는 [0]부터 매겨지기때문에 내가 구하고자 하는 요소의 index 번호가 곧
+# 그 앞에 개수를 나타내는 거지
+# 와 여기까진 그냥 생각 못했을 것 같다 !
