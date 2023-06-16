8a363d58f72282f7ac1ca39c4b2111d7b59df032
JINJOO PARK <117656236+lolo-park@users.noreply.github.com>
2023-06-16 14:39:14 +0900

    자릿수 더하기 divmod()사용 공부

--- programmers/add_digits.py
+++ programmers/add_digits.py
@@ -8,3 +8,24 @@ def solution(n):
     for i in range(len(answer)):
         sum_of_digits += answer[i]
     return sum_of_digits
+
+# 더 간단하게
+def solution(n):
+    answer = sum(list(map(int, str(n))))
+    return answer
+
+# 더 간단하게
+def solution(n):
+    return sum(int(i) for i in str(n))
+
+# while 문을 사용한 풀이
+def solution(n):
+		answer = 0
+		while n:
+			n, r = divmod(n, 10) # n에서 10을 나눈 몫과 나머지를 저장
+			answer += r # answer 변수에 r을 계속 더해나간다
+		return answer
+# n = 1234 라면
+# 1234 // 10 = n 123
+# 1234 % 10 = r 4
+# 1234/10 = 123.4 니까 ! 아하 계속 한자리씩 줄어들게 되는 군...
