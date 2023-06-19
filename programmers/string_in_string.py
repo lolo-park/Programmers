30c48afc529aa93eb57463f72cffc395efe24e07
JINJOO PARK <117656236+lolo-park@users.noreply.github.com>
2023-06-19 09:49:57 +0900

    문자열 안에 문자열-문수샘도움

---
+++ programmers/string_in_string.py
@@ -0,0 +1,14 @@
+"""
+매개변수: 문자열 str1, str2
+조건: str1 안에 str2
+출력: 있으면 1, 없으면 2
+"""
+# 문수샘이 풀어주심
+# 파이썬 내장함수 적극 활용하기
+def solution(str1, str2):
+    if str2 in str1:
+        answer = 1
+        return answer
+    else:
+        answer = 2
+        return answer
